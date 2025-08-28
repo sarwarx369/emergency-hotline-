1469dc7899717518787e48c08678e7a645d5205a
sarwarx369 <golamsarowarzahan@gmail.com>
2025-08-28 22:23:17 +0600

    all button functionality done

--- emergency.js
+++ emergency.js
@@ -35,10 +35,12 @@ for (let i = 0; i < calls.length; i++) {
     });
     let historyList = document.getElementById("history-list");
     let div = document.createElement("div");
-    div.className = "flex justify-between items-center border-b py-2";
-    div.innerHTML = `<span class="font-bold">${title} </span>
-    <span>${number}</span>
-    <span class="text-gray-500 text-sm"> ${time}</span>`;
+    div.className = "flex flex-col m-1 rounded bg-gray-200  py-2";
+    div.innerHTML = `<div class="flex justify-between"><span class="">${title} </span> 
+    <span class="text-gray-500 text-sm"> ${time}</span> 
+    </div>
+    <div class=""> ${number}</div>
+    `;
     historyList.prepend(div);
   });
 }
@@ -47,4 +49,18 @@ let clear = document.getElementById("clear-history");
 clear.addEventListener("click", function () {
   let historyList = document.getElementById("history-list");
   historyList.innerHTML = ``;
+});
+
+let copyButton = document.querySelectorAll(".copy-bt");
+copyButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    let copyCard = button.closest(".fard");
+    let getNumber = copyCard.querySelector(".number").innerText;
+    navigator.clipboard.writeText(getNumber);
+    alert("copied: " + getNumber);
+    let countCopy = document.getElementById("copy-count");
+    let availableCopyCount = parseInt(countCopy.innerText);
+    let newCopies = availableCopyCount + 1;
+    countCopy.innerText = newCopies;
+  });
 });
