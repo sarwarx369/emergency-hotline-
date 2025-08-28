a78198cfff35bb4baf9058a51ca2bd2b26fdc6ab
sarwarx369 <golamsarowarzahan@gmail.com>
2025-08-28 10:03:51 +0600

    call button alert done

--- emergency.js
+++ emergency.js
@@ -8,3 +8,14 @@ for (let i = 0; i < hearts.length; i++) {
     countheart.innerText = newhearts;
   });
 }
+
+let calls = document.getElementsByClassName("call-bt");
+for (let i = 0; i < calls.length; i++) {
+  calls[i].addEventListener("click", function () {
+    let card = this.parentElement.parentElement;
+    let title = card.getElementsByClassName("title")[0].innerText;
+    let number = card.getElementsByClassName("number")[0].innerText;
+    alert("Calling: " + title + "\n" + number);
+    let historylist = document.getElementById("historyLis");
+  });
+}
